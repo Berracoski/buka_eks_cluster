2315c8571fc05bff71cddec648950d9c3879eb07
Pablo Linares <69176408+Berracoski@users.noreply.github.com>
2025-07-16 10:59:50 -0300

    Create ebs-csi.tf

---
+++ ebs-csi.tf
@@ -0,0 +1,8 @@
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name                = aws_eks_cluster.main.name
+  addon_name                  = "aws-ebs-csi-driver"
+  addon_version               = "v1.29.1-eksbuild.1"
+  service_account_role_arn    = aws_iam_role.ebs_csi.arn
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+}
