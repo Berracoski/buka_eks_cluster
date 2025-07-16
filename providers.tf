f233163ec81d14514de326070fa94f436efb03ca
Pablo Linares <69176408+Berracoski@users.noreply.github.com>
2025-07-16 11:13:39 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 provider "kubernetes" {
+  alias                  = "eks"
   host                   = aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.main.token
