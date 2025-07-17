f67c06115071cec9b93bbf1d879aaebce20c1ddb
Pablo Linares <69176408+Berracoski@users.noreply.github.com>
2025-07-17 18:43:13 -0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
+  cidr_block       = "10.0.0.0/16"
+ 
   tags = {
-    Name = "eks-main-vpc"
+    Name = "vpc-proyecto"
   }
 }
 
@@ -11,64 +11,86 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_subnet" "public" {
-  count                   = 3
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  count = 3  
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = {
-    Name = "eks-public-subnet-${count.index + 1}"
+    Name = "public-subnet-${count.index + 1}"
   }
 }
 
-resource "aws_internet_gateway" "igw" {
+resource "aws_subnet" "private" {
+  count = 3  
+  vpc_id = aws_vpc.main.id
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, 3 + count.index)
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "eks-igw"
+    Name = "internet-gw"
   }
 }
 
+#Route table publica
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   tags = {
-    Name = "eks-public-rt"
+    Name = "public"
   }
 }
 
 resource "aws_route" "public_internet_access" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
 }
 
-resource "aws_route_table_association" "a" {
-  count          = 3
-  subnet_id      = aws_subnet.public[count.index].id
+resource "aws_route_table_association" "public" {
+  count = 3
+  subnet_id = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
+}
+
+#Route table privada
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = 3
+  subnet_id = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
 
 
 # Elastic IP for NAT Gateway
 resource "aws_eip" "nat" {
-  count = 3
   vpc   = true
 }
 
-# NAT Gateway in each public subnet
+
 resource "aws_nat_gateway" "nat" {
-  count         = 3
-  allocation_id = aws_eip.nat[count.index].id
-  subnet_id     = aws_subnet.public[count.index].id
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
   tags = {
-    Name = "eks-nat-gateway-${count.index + 1}"
+    Name = "nat-gateway"
   }
 }
 
 # Modify private route tables to route outbound traffic via NAT Gateway
 resource "aws_route" "private_nat_gateway_route" {
-  count                  = 3
-  route_table_id         = aws_route_table.private[count.index].id
+  route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat[count.index].id
+  nat_gateway_id         = aws_nat_gateway.nat.id
 }
